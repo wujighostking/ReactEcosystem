07b40b3be1d10e851224d20c4439ab72c6035bf2
gk <promise_ghostking@163.com>
2025-09-06 18:43:40 +0800

    feat: 处理 workLoop 函数

--- packages/react/packages/scheduler/src/Scheduler.ts
+++ packages/react/packages/scheduler/src/Scheduler.ts
@@ -1,5 +1,8 @@
+import type { Heap } from './SchedulerMinHeap'
 import type { PriorityLevel } from './SchedulerPriorities'
-import { getCurrentTime } from 'shared'
+import { getCurrentTime, isFunction } from 'shared'
+import { peek, pop } from './SchedulerMinHeap'
+import { NoPriority } from './SchedulerPriorities'
 
 type Callback = (...args: any[]) => Callback | undefined | null
 
@@ -11,8 +14,15 @@ export interface Task {
   expirationTime: number
   sortIndex: number
 }
+const taskQueue: Heap<Task> = []
+
+// 当前正在执行的任务
+let currentTask: Task | null = null
+
+let currentPriorityLevel: PriorityLevel = NoPriority
 
 // 记录时间切片的起始值，时间戳
+// eslint-disable-next-line prefer-const
 let startTime = -1
 
 // 时间切片，这是一个时间段
@@ -28,4 +38,35 @@ export function shouldYieldToHost() {
   }
 
   return true
+}
+
+export function workLoop(initialTime: number) {
+  const currentTime = initialTime
+  currentTask = peek(taskQueue)
+
+  // eslint-disable-next-line no-unmodified-loop-condition
+  while (currentTask !== null) {
+    if (currentTask.expirationTime > currentTime && shouldYieldToHost()) {
+      break
+    }
+    const callback = currentTask.callback
+
+    if (isFunction(callback)) {
+      currentTask.callback = null
+      currentPriorityLevel = currentTask.priorityLevel
+      const didUserCallbackTimeout = currentTask.expirationTime <= currentTime
+      const continuationCallback = callback(didUserCallbackTimeout)
+
+      if (isFunction(continuationCallback)) {
+        currentTask.callback = continuationCallback
+      }
+    }
+    else {
+      pop(taskQueue)
+    }
+  }
+}
+
+export function getCurrentPriorityLevel() {
+  return currentPriorityLevel
 }
