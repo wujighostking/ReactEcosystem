df6b3332c8d087810578b3081398490bd9a0afb4
gk <promise_ghostking@163.com>
2025-09-12 18:38:35 +0800

    feat: 任务调度函数入口

--- packages/react/packages/scheduler/src/Scheduler.ts
+++ packages/react/packages/scheduler/src/Scheduler.ts
@@ -1,8 +1,15 @@
 import type { Heap } from './SchedulerMinHeap'
 import type { PriorityLevel } from './SchedulerPriorities'
 import { getCurrentTime, isFunction } from 'shared'
-import { peek, pop } from './SchedulerMinHeap'
-import { NoPriority } from './SchedulerPriorities'
+import {
+  lowPriorityTimeout,
+  maxSigned32BitInt,
+  normalPriorityTimeout,
+  userBlockingPriorityTimeout,
+} from './SchedulerFeatureFlags.js'
+
+import { peek, pop, push } from './SchedulerMinHeap'
+import { IdlePriority, ImmediatePriority, LowPriority, NoPriority, UserBlockingPriority } from './SchedulerPriorities'
 
 type Callback = (...args: any[]) => Callback | undefined | null
 
@@ -28,16 +35,19 @@ let startTime = -1
 // 时间切片，这是一个时间段
 const frameInterval = 5
 
+let taskIdCounter: number = 0
+
+let isHostCallbackScheduled: boolean = false
+
 export function shouldYieldToHost() {
   const timeElapsed = getCurrentTime() - startTime
 
-  // return timeElapsed >= frameInterval
-
-  if (timeElapsed < frameInterval) {
-    return false
-  }
-
-  return true
+  return timeElapsed >= frameInterval
+  // if (timeElapsed < frameInterval) {
+  //   return false
+  // }
+  //
+  // return true
 }
 
 export function workLoop(initialTime: number): boolean {
@@ -73,13 +83,59 @@ export function workLoop(initialTime: number): boolean {
     currentTask = peek(taskQueue)
   }
 
-  if (currentTask !== null) {
-    return true
+  return currentTask !== null
+  // if (currentTask !== null) {
+  //   return true
+  // }
+  // else {
+  //   return false
+  // }
+}
+
+// 任务调度函数入口
+export function scheduleCallback(priorityLevel: PriorityLevel, callback: Callback) {
+  const startTime = getCurrentTime()
+  let timeout: number
+
+  switch (priorityLevel) {
+    case ImmediatePriority:
+      timeout = -1
+      break
+    case UserBlockingPriority:
+      timeout = userBlockingPriorityTimeout
+      break
+    case IdlePriority:
+      timeout = maxSigned32BitInt
+      break
+    case LowPriority:
+      timeout = lowPriorityTimeout
+      break
+    default:
+      timeout = normalPriorityTimeout
+      break
   }
-  else {
-    return false
+
+  const expirationTime = startTime + timeout
+
+  const newTask: Task = {
+    id: taskIdCounter++,
+    callback,
+    priorityLevel,
+    startTime,
+    expirationTime,
+    sortIndex: -1,
+  }
+
+  newTask.sortIndex = expirationTime
+  push(taskQueue, newTask)
+
+  if (!isHostCallbackScheduled) {
+    isHostCallbackScheduled = true
+    requestHostCallback()
   }
 }
+
+function requestHostCallback() {}
 
 export function getCurrentPriorityLevel() {
   return currentPriorityLevel
