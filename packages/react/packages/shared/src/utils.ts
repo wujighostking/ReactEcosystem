8b3583da28cab26f72608bb01101046c3d7147c2
gk <promise_ghostking@163.com>
2025-10-02 11:38:36 +0800

    feat: 添加部分工具函数

--- packages/react/packages/shared/src/utils.ts
+++ packages/react/packages/shared/src/utils.ts
@@ -9,3 +9,19 @@ export function isFunction(val: any): val is (...args: any[]) => any {
 export function isNumber(val: any): val is number {
   return typeof val === 'number'
 }
+
+export function hasChange(nextState: any, prevState: any) {
+  return !Object.is(nextState, prevState)
+}
+
+export function isObject(val: any): val is Record<any, any> {
+  return val !== null && typeof val === 'object'
+}
+
+export function isString(val: any): val is string {
+  return typeof val === 'string'
+}
+
+export const assign = Object.assign
+
+export const isArray = Array.isArray
---
+++ packages/zustand/src/create.ts
@@ -0,0 +1,6 @@
+export function create() {
+  return createImpl()
+}
+
+function createImpl() {
+}
