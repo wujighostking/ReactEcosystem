819016350c08016cd6dfff62dd0273a0e66712b0
gk <promise_ghostking@163.com>
2025-10-02 16:57:10 +0800

    feat: 实现 create 方法初始化 store

---
+++ packages/zustand/__test__/create.spec.ts
@@ -0,0 +1,55 @@
+import { describe, expect, it } from 'vitest'
+import { create } from '../src'
+
+describe('test zustand', () => {
+  const useStore = create(set => ({
+    age: 0,
+    addAge: () => {
+      set((state) => {
+        return {
+          age: state.age + 1,
+        }
+      })
+    },
+
+    minusAge: () => {
+      set((state) => {
+        return {
+          age: state.age - 1,
+        }
+      })
+    },
+  }))
+
+  const store = useStore()
+
+  it('test store', () => {
+    expect(store).toEqual({
+      ...store,
+    })
+  })
+
+  it('create store property', () => {
+    expect(store.getState().age).toBe(0)
+  })
+
+  it('create store method', () => {
+    const { addAge } = store.getState()
+    addAge()
+
+    expect(store.getState().age).toBe(1)
+  })
+
+  it('test zustand create callback', () => {
+    const age = useStore(state => state.age)
+
+    expect(age).toBe(1)
+  })
+
+  it('test zustand set merge function', () => {
+    const { minusAge } = store.getState()
+    minusAge()
+
+    expect(store.getState().age).toBe(0)
+  })
+})
--- packages/zustand/src/create.ts
+++ packages/zustand/src/create.ts
@@ -1,6 +1,44 @@
-export function create() {
-  return createImpl()
+import { assign, hasChange, isFunction } from 'shared'
+
+export type StateSetup = (set: SetState, get: GetState) => Record<any, any>
+type SetState = (setNewState: (state: StoreResult) => StoreResult) => void
+type GetState = () => Record<any, any>
+type StoreResult = ReturnType<StateSetup>
+
+export function create(stateSetup: StateSetup) {
+  return createImpl(stateSetup)
 }
 
-function createImpl() {
+function createImpl(stateSetup: StateSetup) {
+  let state: StoreResult
+
+  const getState = () => state
+
+  const setState = (setNewState: (state: StoreResult) => StoreResult) => {
+    const nextState = setNewState(state)
+    const prevState = state
+
+    if (hasChange(nextState, prevState)) {
+      state = assign({}, prevState, nextState)
+    }
+  }
+
+  state = stateSetup(setState, getState)
+
+  function createStore() {
+    return {
+      ...state,
+
+      getState,
+      setState,
+    }
+  }
+
+  return (get?: (state: StoreResult) => StoreResult) => {
+    if (isFunction(get)) {
+      return get(state)
+    }
+
+    return createStore()
+  }
 }
